35abe538898fbecd9f6b84650a074fea67f16fdd
deepsj1012 <deepsj1012@naver.com>
2024-01-24 12:10:04 +0900

    프로젝트 생성

---
+++ src/main/java/com/reservationpurchase/reservationpurchase/ReservationPurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.reservationpurchase.reservationpurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationPurchaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationPurchaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reservationpurchase/reservationpurchase/ReservationPurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservationpurchase.reservationpurchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPurchaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
